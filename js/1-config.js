bf20dafe5f8d5cc41f7fb74e779e7a3e4f21edc3
Eija-01 <rezaearts@gmail.com>
2026-05-13 01:21:48 +0700

    Create 1-config.js

---
+++ js/1-config.js
@@ -0,0 +1,12 @@
+// 1. CONFIG
+// Fungsi: Berisi variabel pengaturan dasar yang bersifat tetap (konstanta).
+// Fokus ke file ini jika ingin mengubah metrik engine dasar seperti kecepatan jalan karakter, ukuran base karakter, kecepatan lari, tinggi maksimal lompatan, atau skala dunia.
+
+const jumpScale = 0.8; 
+const baseCharSize = 130; 
+const walkSpeed = 2; 
+const runSpeed = 4; 
+const FIXED_ZOOM = 1;
+const jumpDuration = 600; 
+const jumpMaxHeight = 70;
+const angle = 25;
